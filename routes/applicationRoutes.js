7ac89c4b865d30cb9e7e008af1ed723ae768d8f9
hareem09 <hareemmohsin93@gmail.com>
2026-07-03 21:57:03 +0500

    added new api

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -11,5 +11,9 @@ const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Route: Only authenticated users with the 'intern' role can submit an application
 router.post('/apply', protect, authorize('intern'), applicationController.applyToJob);
+router.get('/applications/:user_id',protect,authorize('intern'),applicationController.getMyApplications)
+router.get('/track/:job_id',protect,authorize('intern'),applicationController.trackApplicationStatus)
+router.put('/update/:id',protect,authorize('intern'),applicationController.updateApplicationData)
+router.delete('/delete/:id',protect,authorize('intern'),applicationController.deleteApplicationData)
 
 module.exports = router;
